c91410ee58858d38ea0c91c849499930c05c2ed7
Tal Be'ery <tal.a.beery@gmail.com>
2024-09-29 22:49:10 +0300

    beautified
    beautified

--- static.whatsapp.net/rsrc.php/v3/yL/r/dbgrMsnD18p.js
+++ static.whatsapp.net/rsrc.php/v3/yL/r/dbgrMsnD18p.js
@@ -1,4 +1,11 @@
-;/*FB_PKG_DELIM*/
+; /*FB_PKG_DELIM*/
 
-var dataElement=document.getElementById("envjson");if(dataElement!=null){var copyVariables=function(a){for(var b in variables)a[b]=variables[b]},variables=JSON.parse(dataElement.textContent);window.requireLazy?window.requireLazy(["Env"],copyVariables):(window.Env=window.Env||{},copyVariables(window.Env))}
+var dataElement = document.getElementById("envjson");
+if (dataElement != null) {
+    var copyVariables = function (a) {
+            for (var b in variables) a[b] = variables[b]
+        },
+        variables = JSON.parse(dataElement.textContent);
+    window.requireLazy ? window.requireLazy(["Env"], copyVariables) : (window.Env = window.Env || {}, copyVariables(window.Env))
+}
 //# sourceURL=https://static.whatsapp.net/rsrc.php/v3/yL/r/dbgrMsnD18p.js?_nc_x=Ij3Wp8lg5Kz
