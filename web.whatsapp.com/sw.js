6bd0c83f38bcf0249c535a1bf95c4b218334c2d1
Tal Be'ery <tal.a.beery@gmail.com>
2024-09-29 22:10:57 +0300

    Initial commit

---
+++ web.whatsapp.com/sw.js
@@ -0,0 +1,12 @@
+__DEV__=0;/*FB_PKG_DELIM*/
+self.__swData=JSON.parse(/*BTDS*/"{\"dynamic_data\":{\"dynamic_modules\":{\"cr:4122\":{\"__rc\":[null,null]},\"cr:4324\":{\"__rc\":[null,null]},\"cr:4533\":{\"__rc\":[null,null]},\"cr:4722\":{\"__rc\":[null,null]},\"cr:4941\":{\"__rc\":[null,null]},\"cr:5151\":{\"__rc\":[null,null]},\"cr:5292\":{\"__rc\":[null,null]},\"cr:5411\":{\"__rc\":[null,null]},\"cr:5664\":{\"__rc\":[null,null]},\"cr:6640\":{\"__rc\":[null,null]},\"cr:10197\":{\"__rc\":[null,null]},\"cr:10198\":{\"__rc\":[null,null]},\"IntlCurrentLocale\":{\"code\":\"en_US\"},\"WAWebSwResources\":{\"wa_default_notification_icon\":\"https:\\\/\\\/static.whatsapp.net\\\/rsrc.php\\\/v3\\\/yX\\\/r\\\/JYPizEwERE4.png\"},\"SiteData\":{\"server_revision\":1016820347,\"client_revision\":1016820347,\"push_phase\":\"C3\",\"pkg_cohort\":\"BP:DEFAULT\",\"haste_session\":\"19992.BP:DEFAULT.2.0..0.0\",\"pr\":2,\"manifest_base_uri\":\"https:\\\/\\\/static.whatsapp.net\",\"manifest_origin\":null,\"manifest_version_prefix\":null,\"be_one_ahead\":false,\"is_rtl\":false,\"is_experimental_tier\":false,\"is_jit_warmed_up\":true,\"hsi\":\"7418856402978383899\",\"semr_host_bucket\":\"6\",\"bl_hash_version\":2,\"comet_env\":0,\"wbloks_env\":false,\"ef_page\":null,\"compose_bootloads\":false,\"spin\":0,\"__spin_r\":1016820347,\"__spin_b\":\"trunk\",\"__spin_t\":1727337111,\"vip\":\"157.240.251.60\"}},\"hsdp\":{\"gkxData\":{\"5943\":{\"result\":false,\"hash\":null},\"26256\":{\"result\":false,\"hash\":null},\"26258\":{\"result\":true,\"hash\":null},\"26259\":{\"result\":false,\"hash\":null},\"26262\":{\"result\":true,\"hash\":null}},\"justknobxData\":{\"371\":{\"r\":true},\"1050\":{\"r\":false},\"1617\":{\"r\":159},\"1618\":{\"r\":8},\"1619\":{\"r\":1},\"1620\":{\"r\":2},\"1621\":{\"r\":4},\"1622\":{\"r\":0},\"1623\":{\"r\":6},\"1624\":{\"r\":1},\"1662\":{\"r\":2},\"1663\":{\"r\":14},\"1664\":{\"r\":2},\"1784\":{\"r\":false},\"1854\":{\"r\":false},\"2237\":{\"r\":false},\"2337\":{\"r\":false},\"2434\":{\"r\":true},\"2702\":{\"r\":true}}}}}");/*FB_PKG_DELIM*/
+
+      if (self.trustedTypes && self.trustedTypes.createPolicy) {
+        const escapeScriptURLPolicy = self.trustedTypes.createPolicy("workerPolicy", {
+          createScriptURL: url => url
+        });
+        importScripts(escapeScriptURLPolicy.createScriptURL(/*BTDS*/"https:\/\/static.whatsapp.net\/rsrc.php\/v3\/yY\/r\/zP2YcyK5epv.js?_nc_x=Ij3Wp8lg5Kz"));
+      } else {
+         importScripts(/*BTDS*/"https:\/\/static.whatsapp.net\/rsrc.php\/v3\/yY\/r\/zP2YcyK5epv.js?_nc_x=Ij3Wp8lg5Kz");
+      }
+      
